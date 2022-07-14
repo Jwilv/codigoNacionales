fbe4c39de09b5b1baf5f9d46b6200ed004245c7e
Jwilv <100983338+Jwilv@users.noreply.github.com>
2022-07-14 15:00:19 -0300

    PARAMATI

--- lineas1v.ino
+++ lineas1v.ino
@@ -1,1 +1,18 @@
-
+#define DEGUG 1
+int sensorDer = xx;
+int sensorIzq = xx;
+int recepDer = xx;
+int recepIzq = xx;
+void setup(){
+    Serial.begin(9600);
+}
+void loop(){
+recepDer = digitalRead(sensorDer);
+recepIzq = digitalRead(sensorIzq);
+ if(DEGUG){
+    Serial.print("sensor derecho");
+    Serial.println(recepDer);
+    Serial.print("sensor izquierod");
+    Serial.println(recepIzq);
+    }
+}
